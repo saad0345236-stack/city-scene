1ff752e835616c8adc46ac0131edcec4f3aaf77a
saad0345236-stack <saad0345236@gmail.com>
2026-07-02 19:59:01 +0500

    Create practice_0002.py

---
+++ practice_0002.py
@@ -0,0 +1,33 @@
+# Declaring variables:
+number = 100
+number = 110
+print(number)
+decimal_value = 2.5
+print(decimal_value)
+name = 'Tom'
+boolean_value = True
+print(boolean_value)
+# Exercise:
+name = 'John Smith'
+age = 20
+existing_patient = False
+# Using input function:
+name = input("What's your name? ")
+print("Hi " + name)
+# Exercise:
+person_name = input('What is your name? ')
+print('Hi ' + person_name)
+color = input("What's your favorite color? ")
+print(person_name + ' likes ' + color)
+# More practice:
+variable = "value"
+print(variable)
+variable = "new_value"
+print(variable)
+new_variable = input('What is the new value? ')
+print(new_variable + ' is the new value')
+# Using boolean function:
+is_student = True
+is_teacher = False
+print(is_student)
+print(is_teacher)

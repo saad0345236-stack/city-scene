ad6b8f8d512ae8c53e0f465d4c9807d9a2a26487
saad0345236-stack <saad0345236@gmail.com>
2026-07-05 19:47:06 +0500

    Create shape_arithmetic_calculator.py

---
+++ shape_arithmetic_calculator.py
@@ -0,0 +1,19 @@
+# Asking 2 numbers from the user:
+print("There's a rectangle...")
+print("Pick the height and width of a rectangle.")
+X = int(input("Height: "))
+Y = int(input("Width: "))
+print("You'll now see 4 different arithmetic operational results on the screen. As we proceed with the process.")
+A = X + Y
+print("Addition: ")
+print(A)
+B = X - Y
+print("Subtraction: ")
+print(B)
+C = X * Y
+print("Multiplication: ")
+print(C)
+D = X / Y
+print("Divison: ")
+print(D)
+print("Process completed.")

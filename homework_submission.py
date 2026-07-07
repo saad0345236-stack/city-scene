1dad41ab8d47f78b1382c838253c0d2cc3f5594e
saad0345236-stack <saad0345236@gmail.com>
2026-07-07 19:38:51 +0500

    Create homework_submission.py

---
+++ homework_submission.py
@@ -0,0 +1,7 @@
+# Asking a student for homework:
+homework = input("Did you do your homework? ")
+length = input("Is it 1 page long? ")
+if homework == "yes" and length == "yes":
+    print("Good job.")
+else:
+    print("Complete your homework.")
